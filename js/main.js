03b531f5f2db47aafdf9952f67f7098609c60568
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-02-06 11:56:18 +0700

    update index

--- js/main.js
+++ js/main.js
@@ -2,39 +2,39 @@ $(document).ready(function () {
     $(window).scroll(function () {
         // sticky navbar on scroll script
         if (this.scrollY > 20) {
-            $('.header').addClass("sticky");
+            $(".header").addClass("sticky");
         } else {
-            $('.header').removeClass("sticky");
+            $(".header").removeClass("sticky");
         }
 
         // scroll-up button show/hide script
         if (this.scrollY > 20) {
-            $('.scroll-up-btn').addClass("show");
+            $(".scroll-up-btn").addClass("show");
         } else {
-            $('.scroll-up-btn').removeClass("show");
+            $(".scroll-up-btn").removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function () {
-        $('html').animate({
-            scrollTop: 0
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({
+            scrollTop: 0,
         });
         // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+        $("html").css("scrollBehavior", "auto");
     });
 
     $(".navbar .menu li a").click(function () {
         // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-        $('.navbar .menu li a').removeClass("nav-active");
+        $("html").css("scrollBehavior", "smooth");
+        $(".navbar .menu li a").removeClass("nav-active");
         $(this).addClass("nav-active");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
     });
 
     // typing text animation script
@@ -42,25 +42,25 @@ $(document).ready(function () {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     var typed = new Typed(".typing-2", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     var typed = new Typed(".typing-name", {
         strings: ["Nguyen Thanh Nam"],
         typeSpeed: 80,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
+    $(".carousel").owlCarousel({
         autoplay: true,
         autoplayTimeout: 3000,
         autoplaySpeed: 1500,
@@ -70,15 +70,15 @@ $(document).ready(function () {
         dot: true,
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
             600: {
-                items: 2
+                items: 2,
             },
             1024: {
-                items: 3
-            }
-        }
+                items: 3,
+            },
+        },
     });
 });
 // current realtime
@@ -95,16 +95,14 @@ function renderTime() {
     var monthDob = dob.getMonth();
     var dateDob = dob.getDate();
 
-    // processing 
+    // processing
     var yearAge = yearNow - yearDob;
-    if (monthNow >= monthDob)
-        var monthAge = monthNow - monthDob;
+    if (monthNow >= monthDob) var monthAge = monthNow - monthDob;
     else {
         yearNow--;
         var monthAge = 12 + monthNow - monthDob;
     }
-    if (dateNow >= dateDob)
-        var dateAge = dateNow - dateDob;
+    if (dateNow >= dateDob) var dateAge = dateNow - dateDob;
     else {
         monthAge--;
         var dateAge = 31 + dateNow - dateDob;
@@ -123,15 +121,11 @@ function renderTime() {
     if (h == 24) {
         h = 0;
     } else {
-        if (h > 12)
-            h -= 0;
+        if (h > 12) h -= 0;
     }
-    if (h < 10)
-        h = "0" + h;
-    if (m < 10)
-        m = "0" + m;
-    if (s < 10)
-        s = "0" + s;
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
 
     var myYears = document.getElementById("years");
     var myMonths = document.getElementById("months");
@@ -154,28 +148,31 @@ function renderTime() {
     myMinutes.style.color = `red`;
     mySeconds.style.color = `red`;
 
-
     setTimeout("renderTime()", 1000);
 }
 renderTime();
 
 // init cursor
 var cursors = [{
-    "cursor_id": "3",
-    "cursor_type": "0",
-    "cursor_shape": "11",
-    "cursor_image": "",
-    "default_cursor": "auto",
-    "hover_effect": "plugin",
-    "body_activation": "1",
-    "element_activation": "0",
-    "selector_type": "tag",
-    "selector_data": "body",
-    "color": "#f72c26",
-    "width": "30",
-    "blending_mode": "normal"
-}];
+    cursor_id: "3",
+    cursor_type: "0",
+    cursor_shape: "11",
+    cursor_image: "",
+    default_cursor: "auto",
+    hover_effect: "plugin",
+    body_activation: "1",
+    element_activation: "0",
+    selector_type: "tag",
+    selector_data: "body",
+    color: "#f72c26",
+    width: "30",
+    blending_mode: "normal",
+}, ];
 
 function alertCV() {
-    swal("Opps, 404", "Sorry! You can't download CV, please try the next time. Thank you!!!", "error");
+    swal(
+        "Opps, 404",
+        "Sorry! You can't download CV, please try the next time. Thank you!!!",
+        "error"
+    );
 }
