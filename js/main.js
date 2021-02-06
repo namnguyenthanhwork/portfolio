a95c7af95241fc4ddd300a99b5fe38fe2bdddd1b
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-02-06 22:36:00 +0700

    update index

--- js/main.js
+++ js/main.js
@@ -39,14 +39,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
